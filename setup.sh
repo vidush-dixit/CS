5722e31d21c7fc21ff867f79813b596a79f87363
Vidush Dixit <32012141+vidush-dixit@users.noreply.github.com>
2020-07-06 22:30:49 +0530

    Intial

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
-mkdir -p ~/.streamlit/
-echo “\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = \n\
-“ > ~/.streamlit/config.toml
+mkdir -p ~/.streamlit
+
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
+" > ~/.streamlit/config.toml
